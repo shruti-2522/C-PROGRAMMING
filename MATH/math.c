413acee0924695510b1fec98acbd698d4178dc7d
shruti amrutkar <shrutiamrutkar177@gmail.com>
2021-03-03 18:54:15 +0530

    Math functions

---
+++ MATH/math.c
@@ -0,0 +1,28 @@
+//MATH FUNCTIONSS:
+#include<stdio.h>  
+#include <math.h>    
+int main(){    
+printf("\n%f",ceil(3.6));    
+printf("\n%f",ceil(3.3));    
+printf("\n%f",floor(3.6));    
+printf("\n%f",floor(3.2));    
+printf("\n%f",sqrt(16));    
+printf("\n%f",sqrt(7));    
+printf("\n%f",pow(2,4));    
+printf("\n%f",pow(3,3));    
+printf("\n%d",abs(-12));     
+ return 0;    
+}    
+
+/*OUTPUT:
+
+4.000000
+4.000000 
+3.000000 
+3.000000 
+4.000000 
+2.645751 
+16.000000
+27.000000
+12       
+*/
